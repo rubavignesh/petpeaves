27a4075cb722e75b3462d4852c1c112b679d7016
rashmidkulkarni <rashmidk1234@vt.edu>
2023-11-25 22:29:34 -0500

    added new files for dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
 import LoginForm from "./components/Login&SignUp/LoginForm";
 import SignUp from "./components/Login&SignUp/SignUp";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<LoginForm />} />
-          <Route path="/signup" element={<SignUp />} />
+          <Route path = "/dashboard" element = {<Dashboard />} />
           <Route path="*" element={<div>Page Not Found</div>} />
         </Routes>
       </Router>
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,7 +1,13 @@
+import Dashboard from "../Dashboard/Dashboard";
+import Sidebar from "../Dashboard/Sidebar";
+import AppointMentForm from "../Appointment/AppointMentForm/AppointMentForm";
 const Home = () => {
   return (
     <div className="home">
-      <h2>Home</h2>
+    <Sidebar></Sidebar>
+    <Dashboard></Dashboard>
+    <AppointMentForm></AppointMentForm>
+
     </div>
   );
 };
