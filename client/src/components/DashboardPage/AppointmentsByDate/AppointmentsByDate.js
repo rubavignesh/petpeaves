a22d2432c2171d71766fde3325cfc668f7e3dddc
mehtaamisha <142864251+mehtaamisha@users.noreply.github.com>
2023-11-27 20:25:34 -0500

    dashboard binding and extra files removed

--- client/src/components/DashboardPage/AppointmentsByDate/AppointmentsByDate.js
+++ client/src/components/DashboardPage/AppointmentsByDate/AppointmentsByDate.js
@@ -4,73 +4,89 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './AppointmentsByDate.css';
 import AppointmentsTable from './AppointmentsTable';
+import { useAuthContext } from '../../../hooks/useAuthContext.js';
 
 const AppointmentList = () => {
     const [selectedDate, setSelectedDate] = useState(new Date());
-    const [appointments, setAppointments] = useState([{
-        "_id": {
-          "$oid": "6564fd1d805268aeede1c59f"
-        },
-        "username": "Ruba",
-        "phone": "5716357859",
-        "email": "rubavignesh@vt.edu",
-        "pet": "Dog",
-        "age": "2",
-        "weight": "34",
-        "serviceType": "Grooming",
-        "appointmentDate": "Mon Nov 27 2023",
-        "appointmentTime": "10:00 AM",
-        "doctor_id": "60a7c4b5a1c6a71b7c4b5a1c",
-        "user_id": "6563f24601265027cfa6c689",
-        "createdAt": {
-          "$date": "2023-11-27T20:33:33.683Z"
-        },
-        "updatedAt": {
-          "$date": "2023-11-27T20:33:33.683Z"
-        },
-        "__v": 0
-      }]);
-    // Uncomment the lines below when you have the loggedInUser context available
-    // const { loggedInUser } = useContext(UserContext);
+    const [appointments, setAppointments] = useState([]);
+    const { user } = useAuthContext();
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     const handleAppointmentDate = (date) => {
         setSelectedDate(date);
     }
 
-    // useEffect(() => {
-    //     fetch('http://localhost:5000/appointmentsByDate', {
-    //         method: 'POST',
-    //         headers: { 'Content-type': 'application/json' },
-    //         body: JSON.stringify({ date: selectedDate.toDateString(), email: loggedInUser.email })
-    //     })
-    //         .then(res => res.json())
-    //         .then(data => setAppointments(data))
-    // }, [selectedDate])
+    useEffect(() => {
+        const fetchAppointments = async () => {
+            try {
+                setLoading(true);
+
+                const response = await fetch('http://localhost:4000/api/appointments/appointmentByDate',
+                    {
+                        method: 'POST',
+                        headers: {
+                            'Content-type': 'application/json',
+                            'Authorization': `Bearer ${user?.token}`
+                        },
+                        body: JSON.stringify({ appointmentDate: selectedDate.toDateString() })
+                    })
+                const appointmentRes = await response.json();
+
+                if (!response.ok) {
+                    setLoading(false);
+                    setError(appointmentRes.error);
+                }
+                if (response.ok) {
+                    setAppointments(appointmentRes);
+                }
+            } catch (error) {
+                setError('Error fetching available appointments:' + error.message);
+                setLoading(false);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        if (selectedDate && user) {
+            fetchAppointments();
+        }
+    }, [selectedDate, user]);
 
     return (
-        <Grid container spacing={3}>
-            <Grid item xs={12} sm={4}>
-                <h1 style={{ marginBottom: '5rem', marginLeft: '6rem', color: "rgb(5, 99, 141)", fontFamily: "initial"  }}>Appointment Calendar</h1>
-                <div style={{ marginBottom: '5rem', marginLeft: '4rem' }}>
-                    <Calendar
-                        onChange={handleAppointmentDate}
-                        value={selectedDate}
-                    />
-                    <br></br>
-                </div>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-                <div className="appointments-date-heading">
-                <h2 style={{color: "rgb(5, 99, 141)", fontFamily: "initial"}}>Total Appointments: {appointments.length}</h2>
-                <h3 style={{color: "rgb(5, 99, 141)", fontFamily: "initial"}}>{selectedDate.toDateString()}</h3>
-                </div>
-                {
-                    appointments.length === 0
-                        ? <Typography variant="body2" align="center">There are no appointments on this date</Typography>
-                        : <AppointmentsTable appointments={appointments}></AppointmentsTable>
-                }
-            </Grid>
-        </Grid>
+        <div>
+            {user ? (
+                <>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12} sm={4}>
+                            <h1 style={{ marginBottom: '5rem', marginLeft: '6rem', color: "rgb(5, 99, 141)", fontFamily: "initial" }}>Appointment Calendar</h1>
+                            <div style={{ marginBottom: '5rem', marginLeft: '4rem' }}>
+                                <Calendar
+                                    onChange={handleAppointmentDate}
+                                    value={selectedDate}
+                                />
+                                <br></br>
+                            </div>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <div className="appointments-date-heading">
+                                <h2 style={{ color: "rgb(5, 99, 141)", fontFamily: "initial" }}>Total Appointments: {appointments.length}</h2>
+                                <h3 style={{ color: "rgb(5, 99, 141)", fontFamily: "initial" }}>{selectedDate.toDateString()}</h3>
+                            </div>
+                            {
+                                appointments.length === 0
+                                    ? <Typography variant="body2" align="center">There are no appointments on this date</Typography>
+                                    : <AppointmentsTable appointments={appointments}></AppointmentsTable>
+                            }
+                        </Grid>
+                    </Grid>
+                    {error && <div className="error">{error}</div>}
+                    {loading && <div className="loading">Loading...</div>}
+                </>
+            ) : (
+                <p>Loading...</p>
+            )}
+        </div>
     );
 };
 
